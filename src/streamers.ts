6eecd07f5b2dff44c8cb83a6a0548a498c610704
xelnia <xelnia@hotmail.com>
2022-03-26 19:46:55 -0700

    more updates

--- src/streamers.ts
+++ src/streamers.ts
@@ -1,13 +1,16 @@
 import type { Streamer } from "./streamer.model";
 
 export const streamers: Streamer[] = [
-  { twitchUsername: "xelnia", dkfUsername: "xelnia" },
-  { twitchUsername: "WesCopeland", dkfUsername: "wc" },
-  { twitchUsername: "muscleandfitness", dkfUsername: "muscleandfitness" },
-  { twitchUsername: "rayfinkel", dkfUsername: "rayfinkel" },
+  { twitchUsername: "LordBBH", dkfUsername: "BBH" }, 
   { twitchUsername: "futura1978" },
-  { twitchUsername: "up2ng", dkfUsername: "up2ng" },
-  { twitchUsername: "LordBBH", dkfUsername: "BBH" },
-  { twitchUsername: "Prow7", dkfUsername: "Prow" },
+  { twitchUsername: "Jeffw356" },
+  { twitchUsername: "Lmdave", dkfUsername: "LMDAVE" },
   { twitchUsername: "twitchshane", dkfUsername: "mameshane" },
+  { twitchUsername: "muscleandfitness", dkfUsername: "muscleandfitness" },
+  { twitchUsername: "Prow7", dkfUsername: "Prow" },
+  { twitchUsername: "rayfinkel", dkfUsername: "rayfinkel" },
+  { twitchUsername: "up2ng", dkfUsername: "up2ng" },
+  { twitchUsername: "dwwnp", dkfUsername: "VON" }, 
+  { twitchUsername: "WesCopeland", dkfUsername: "wc" },
+  { twitchUsername: "xelnia", dkfUsername: "xelnia" },
 ];
