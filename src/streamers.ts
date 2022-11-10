5ee33c6eabd09107a9dd70fa7f22102e8f9c5cc2
Wes Copeland <wlcopeland1@gmail.com>
2022-11-10 17:14:24 -0500

    Update streamers.ts

--- src/streamers.ts
+++ src/streamers.ts
@@ -238,7 +238,7 @@ export const streamers: Streamer[] = [
   { twitchUsername: "VeryApe121", dkfUsername: "VeryApe121" },
   { twitchUsername: "weiwovaseiba", dkfUsername: "weiwovaseiba" },
   { twitchUsername: "WesCopeland", dkfUsername: "wc" },
-  { twitchUsername: "RealBigGamer", dkfUsername: "wc" },
+  { twitchUsername: "RealBigGaming", dkfUsername: "wc" },
   { twitchUsername: "WFLImusic", dkfUsername: "wflimusic" },
   { twitchUsername: "wv_pinball", dkfUsername: "wvapinball" },
   { twitchUsername: "wwdkong", dkfUsername: "ww" },
